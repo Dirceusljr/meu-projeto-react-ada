fa03ac51fa4da83bbc61c67ca0bc6b478e9c7af9
Dirceusljr <dirjunior18@gmail.com>
2023-11-11 17:44:54 -0300

    primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import MeuContador from "./Components/MeuContador";
+import MinhaLista from "./Components/MinhaLista";
+
+function App() {
+  return (
+    <div>
+      <h1>Projeto 1</h1>
+      <MeuContador />
+
+      <hr></hr>
+      <h1>Projeto 2</h1>
+      <MinhaLista />
+    </div>
+  );
+}
+
+export default App;
